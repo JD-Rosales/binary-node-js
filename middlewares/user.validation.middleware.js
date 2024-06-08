c8f6ef7898a211e1be1d2373a4d98557dd600364
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-08 18:44:56 +0800

    feat: + added User email and phone number validation if it exist in database

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,6 +1,7 @@
 import { USER } from '../models/user.js';
 import { fieldValidator } from '../helpers/validationHelper.js';
 import { sendJSONResponse } from '../helpers/errorResponseHelper.js';
+import { userService } from '../services/userService.js';
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
@@ -17,6 +18,24 @@ const createUserValid = (req, res, next) => {
   const error = fieldValidator({ rulesField, modelObject: USER, reqBody });
 
   if (error) return sendJSONResponse(res, { message: error, code: 400 });
+
+  // check if email is already in use
+  const emailExist = userService.search({ email: reqBody.email });
+  if (emailExist)
+    return sendJSONResponse(res, {
+      message: 'Email is already in use.',
+      code: 400,
+    });
+
+  // check if phone number is already in use
+  const phoneNumberExist = userService.search({
+    phoneNumber: reqBody.phoneNumber,
+  });
+  if (phoneNumberExist)
+    return sendJSONResponse(res, {
+      message: 'Phone number is already in use.',
+      code: 400,
+    });
 
   next();
 };
