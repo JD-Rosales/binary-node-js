ff76aa70f76bf26acbf81a50573ad152b79d7aa8
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-08 18:46:49 +0800

    feat: + added user create service and route handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,26 @@
-import { Router } from "express";
-import { userService } from "../services/userService.js";
+import { Router } from 'express';
+import { userService } from '../services/userService.js';
 import {
   createUserValid,
   updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
-import { responseMiddleware } from "../middlewares/response.middleware.js";
+} from '../middlewares/user.validation.middleware.js';
+import { responseMiddleware } from '../middlewares/response.middleware.js';
 
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.post(
+  '/',
+  createUserValid,
+  async (req, res, next) => {
+    const reqBody = req.body;
+    const user = await userService.create(reqBody);
+
+    res.result = user;
+
+    next();
+  },
+  responseMiddleware
+);
 
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,15 @@
-import { userRepository } from "../repositories/userRepository.js";
+import { userRepository } from '../repositories/userRepository.js';
 
 class UserService {
   // TODO: Implement methods to work with user
+
+  create(data) {
+    const item = userRepository.create(data);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
 
   search(search) {
     const item = userRepository.getOne(search);
