c233e4ee12a4c20bd554283338e733f6af0cbc96
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-08 13:20:17 +0800

    feat: + Done createUser validation middleware

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,7 +1,29 @@
-import { USER } from "../models/user.js";
+import { USER } from '../models/user.js';
+import { fieldValidator } from '../helpers/validationHelper.js';
+import { sendJSONResponse } from '../helpers/errorResponseHelper.js';
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
+
+  const ruleFields = {
+    firstName: { required: true },
+    lastName: { required: true },
+    email: { required: true, email: true },
+    phoneNumber: { required: true, phoneNumber: true },
+    password: { required: true, minLength: 3 },
+  };
+
+  const error = fieldValidator(ruleFields, {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    password,
+  });
+
+  if (error) return sendJSONResponse(res, { message: error, code: 400 });
+
   next();
 };
 
