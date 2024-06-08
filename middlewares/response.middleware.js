575d708b494f0155ee2aa33da875f49dc641c2f0
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-08 18:45:50 +0800

    feat: + added response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,15 @@
+import { sendJSONResponse } from '../helpers/errorResponseHelper.js';
+
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
-  next();
+  if (res.result) {
+    return res.status(200).json(res.result);
+  }
+
+  return sendJSONResponse(res, {
+    message: 'requested data not found.',
+    status: 404,
+  });
 };
 
 export { responseMiddleware };
