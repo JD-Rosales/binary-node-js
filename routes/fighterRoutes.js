bd7d5f5fad0a750b3972f814b85ebae0c84ee735
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-09 17:00:47 +0800

    feat: + Added POST /api/fighters route handle and service

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,13 +1,26 @@
-import { Router } from "express";
-import { fighterService } from "../services/fighterService.js";
-import { responseMiddleware } from "../middlewares/response.middleware.js";
+import { Router } from 'express';
+import { fighterService } from '../services/fighterService.js';
+import { responseMiddleware } from '../middlewares/response.middleware.js';
 import {
   createFighterValid,
   updateFighterValid,
-} from "../middlewares/fighter.validation.middleware.js";
+} from '../middlewares/fighter.validation.middleware.js';
 
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.post(
+  '/',
+  createFighterValid,
+  async (req, res, next) => {
+    const reqBody = req.body;
+    const user = await fighterService.create(reqBody);
+
+    res.data = user;
+
+    next();
+  },
+  responseMiddleware
+);
 
 export { router };
--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,15 @@
-import { fighterRepository } from "../repositories/fighterRepository.js";
+import { fighterRepository } from '../repositories/fighterRepository.js';
 
 class FighterService {
   // TODO: Implement methods to work with fighters
+
+  create(data) {
+    const item = fighterRepository.create(data);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
 }
 
 const fighterService = new FighterService();
