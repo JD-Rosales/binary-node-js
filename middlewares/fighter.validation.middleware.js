11e34d4b56fc3250560bb3aa1a520960a06dcf41
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-09 18:40:33 +0800

    feat: + fighter validation on fighter update

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -41,6 +41,56 @@ const createFighterValid = (req, res, next) => {
 
 const updateFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during update
+  const reqBody = req.body;
+
+  const fighterFields = Object.keys(FIGHTER);
+  const reqFields = Object.keys(reqBody);
+
+  // Check if at least one field from USER object is present in reqBody
+  const hasValidField = fighterFields.some((field) =>
+    reqFields.includes(field)
+  );
+  if (!hasValidField) {
+    return sendJSONResponse(res, {
+      message: 'Request body is empty.',
+      code: 400,
+    });
+  }
+
+  // dynamically add rules if object key is present in the reqBody
+  const rulesField = {
+    ...(reqBody.name && { name: { required: true } }),
+    ...(req.body.health && {
+      health: {
+        optional: true,
+        default: 85,
+        type: 'number',
+        minNumber: 80,
+        maxNumber: 120,
+      },
+    }),
+    ...(reqBody.power && {
+      power: { required: true, type: 'number', minNumber: 1, maxNumber: 100 },
+    }),
+    ...(reqBody.defense && {
+      defense: { required: true, type: 'number', minNumber: 1, maxNumber: 10 },
+    }),
+  };
+
+  const error = fieldValidator({ rulesField, modelObject: FIGHTER, reqBody });
+
+  if (error) return sendJSONResponse(res, { message: error, code: 400 });
+
+  // Validate if fighter name is already in use in the database
+  if (reqBody.name) {
+    const isNameValid = fighterService.search({ name: reqBody.name });
+    if (isNameValid)
+      return sendJSONResponse(res, {
+        message: `fighter name ${reqBody.name} is not available.`,
+        code: 400,
+      });
+  }
+
   next();
 };
 
