a3e193c60f62f43ce3f64e7c90c73ac53bebcbd5
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-09 16:58:41 +0800

    feat: + added number and optional validation utilities

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,7 +1,28 @@
-import { FIGHTER } from "../models/fighter.js";
+import { FIGHTER } from '../models/fighter.js';
+import { fieldValidator } from '../helpers/validationHelper.js';
+import { sendJSONResponse } from '../helpers/errorResponseHelper.js';
 
 const createFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during creation
+  const reqBody = req.body;
+
+  const rulesField = {
+    name: { required: true },
+    health: {
+      optional: true,
+      default: 85,
+      type: 'number',
+      minNumber: 80,
+      maxNumber: 120,
+    },
+    power: { required: true, type: 'number', minNumber: 1, maxNumber: 100 },
+    defense: { required: true, type: 'number', minNumber: 1, maxNumber: 10 },
+  };
+
+  const error = fieldValidator({ rulesField, modelObject: FIGHTER, reqBody });
+
+  if (error) return sendJSONResponse(res, { message: error, code: 400 });
+
   next();
 };
 
