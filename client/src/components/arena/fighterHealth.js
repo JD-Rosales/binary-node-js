148f682e338ed2506a84081d5e2e7f350a1aeed7
JD-Rosales <rosales.jake1999@gmail.com>
2024-06-11 10:48:16 +0800

    feat: + Added fight functionality from previous task

--- client/src/components/arena/fighterHealth.js
+++ client/src/components/arena/fighterHealth.js
@@ -3,16 +3,17 @@ import './arena.css';
 
 export default function FighterHealth({ maxHealth, currentHealth }) {
   const healthPercentage = (currentHealth / maxHealth) * 100;
+  const healthPercentageText = Math.round(healthPercentage);
 
   return (
     <div className='health-bar__container'>
       <div
         className='health-bar__inner'
-        style={{ width: `${healthPercentage}%` }}
+        style={{ width: `${healthPercentage > 0 ? healthPercentage : 0}%` }}
       ></div>
-      <span className='health-bar__text'>{`${Math.round(
-        healthPercentage
-      )}%`}</span>
+      <span className='health-bar__text'>{`${
+        healthPercentageText > 0 ? healthPercentageText : 0
+      }%`}</span>
     </div>
   );
 }
--- client/src/components/arena/index.js
+++ client/src/components/arena/index.js
@@ -1,13 +1,159 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './arena.css';
 import FighterHealth from './fighterHealth';
-import { getHitPower, getBlockPower, getDamage } from './fight';
+import { getDamage, checkCriticalStrike } from './fight';
+import pressKeys from '../../helpers/keyPressHelper';
+import controls from '../../constants/controls';
 
 export default function Arena({ fighter1, fighter2 }) {
-  const [health, setHealth] = useState({
+  const keys = pressKeys();
+  const [currentHealth, setCurrentHealth] = useState({
     fighter1: fighter1.health,
     fighter2: fighter2.health,
   });
+  const [fightLog, setFightLog] = useState([]);
+
+  const currentHealthRef = useRef(currentHealth);
+
+  const handleAttack = ({ attacker, defender, defenderCurrentHealth }) => {
+    const damage = getDamage(attacker, defender);
+    const health = defenderCurrentHealth - damage;
+
+    const logEntry = {
+      [`${attacker.name}Shot`]: damage,
+      [`${defender.name}Health`]: health >= 0 ? health : 0,
+    };
+    setFightLog((prev) => [...prev, logEntry]);
+
+    return health;
+  };
+
+  function handleCriticalAttack({ attacker, defender, defendercurrentHealth }) {
+    const damage = 2 * attacker.attack;
+    const health = defendercurrentHealth - damage;
+
+    const logEntry = {
+      [`${attacker.name}Shot`]: damage,
+      [`${defender.name}Health`]: health >= 0 ? health : 0,
+    };
+    setFightLog((prev) => [...prev, logEntry]);
+
+    return health;
+  }
+
+  const keyUpHandler = (e) => {
+    keys.delete(e.code);
+  };
+
+  const keyDownHandler = (e) => {
+    //
+    if (
+      currentHealthRef.current.fighter1 <= 0 ||
+      currentHealthRef.current.fighter2 <= 0
+    )
+      return;
+
+    keys.set(e.code, true);
+
+    let playerOneCriticalCooldown = false;
+    let playerTwoCriticalCooldown = false;
+
+    switch (e.code) {
+      case controls.PlayerOneAttack:
+        // prevent attacking if either player is blocking
+        if (
+          keys.data.has(controls.PlayerOneBlock) ||
+          keys.data.has(controls.PlayerTwoBlock)
+        )
+          break;
+        const playerTwoHealth = handleAttack({
+          attacker: fighter1,
+          defender: fighter2,
+          defenderCurrentHealth: currentHealthRef.current.fighter2,
+        });
+        setCurrentHealth((prev) => ({ ...prev, fighter2: playerTwoHealth }));
+        break;
+      case controls.PlayerTwoAttack:
+        // prevent attacking if either player is blocking
+        if (
+          keys.data.has(controls.PlayerOneBlock) ||
+          keys.data.has(controls.PlayerTwoBlock)
+        )
+          break;
+        const playerOneHealth = handleAttack({
+          attacker: fighter2,
+          defender: fighter1,
+          defenderCurrentHealth: currentHealthRef.current.fighter1,
+        });
+        setCurrentHealth((prev) => ({ ...prev, fighter1: playerOneHealth }));
+        break;
+      default:
+        break;
+    }
+
+    // fighter1 critical strike
+    if (
+      checkCriticalStrike(
+        keys.data,
+        controls.PlayerOneCriticalHitCombination
+      ) &&
+      !playerOneCriticalCooldown
+    ) {
+      const playerTwoHealth = handleCriticalAttack({
+        attacker: fighter1,
+        defender: fighter2,
+        defenderCurrentHealth: currentHealthRef.current.fighter2,
+      });
+      setCurrentHealth((prev) => ({ ...prev, fighter2: playerTwoHealth }));
+      playerOneCriticalCooldown = true;
+      setTimeout(() => {
+        playerOneCriticalCooldown = false;
+      }, 10000);
+    }
+
+    // fighter2 critical strike
+    if (
+      checkCriticalStrike(
+        keys.data,
+        controls.PlayerTwoCriticalHitCombination
+      ) &&
+      !playerTwoCriticalCooldown
+    ) {
+      const playerOneHealth = handleCriticalAttack({
+        attacker: fighter2,
+        defender: fighter1,
+        defenderCurrentHealth: currentHealthRef.current.fighter1,
+      });
+      setCurrentHealth((prev) => ({ ...prev, fighter1: playerOneHealth }));
+      playerTwoCriticalCooldown = true;
+      setTimeout(() => {
+        playerTwoCriticalCooldown = false;
+      }, 10000);
+    }
+  };
+
+  useEffect(() => {
+    if (currentHealth.fighter1 <= 0) {
+      alert(`${fighter2.name} Wins`);
+    }
+
+    if (currentHealth.fighter2 <= 0) {
+      alert(`${fighter1.name} Wins`);
+    }
+  }, [currentHealth]);
+
+  useEffect(() => {
+    currentHealthRef.current = currentHealth;
+  }, [currentHealth]);
+
+  useEffect(() => {
+    window.addEventListener('keyup', keyUpHandler);
+    window.addEventListener('keydown', keyDownHandler);
+    return () => {
+      window.removeEventListener('keyup', keyUpHandler);
+      window.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
 
   return (
     <div id='arena-wrapper'>
@@ -17,7 +163,7 @@ export default function Arena({ fighter1, fighter2 }) {
           <span className='fighter__name'>{fighter1.name}</span>
           <FighterHealth
             maxHealth={fighter1.health}
-            currentHealth={health.fighter1}
+            currentHealth={currentHealth.fighter1}
           />
         </div>
         <img
@@ -26,6 +172,7 @@ export default function Arena({ fighter1, fighter2 }) {
           }}
           src='https://media1.tenor.com/m/ux2ZycBoVIYAAAAC/storm-x_men.gif'
           height={450}
+          alt='Fighter'
         />
       </div>
       <div id='fighter2-wrapper'>
@@ -33,7 +180,7 @@ export default function Arena({ fighter1, fighter2 }) {
           <span className='fighter__name'>{fighter2.name}</span>
           <FighterHealth
             maxHealth={fighter2.health}
-            currentHealth={health.fighter2}
+            currentHealth={currentHealth.fighter2}
           />
         </div>
         <img
@@ -43,8 +190,11 @@ export default function Arena({ fighter1, fighter2 }) {
           }}
           src='https://media1.tenor.com/m/ux2ZycBoVIYAAAAC/storm-x_men.gif'
           height={450}
+          alt='Fighter'
         />
       </div>
     </div>
   );
 }
+
+// TODO: Refactor this component
